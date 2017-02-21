16f504f5f5f0eb05742e56a21528471c152c888a
Ian Dellbridge <ian.dellbridge@bashton.com>
2017-02-21 09:47:48 +0000

    Add support for 25 Drives , tidied formatting

--- securitygroups.tf
+++ securitygroups.tf
@@ -3,147 +3,145 @@ resource "aws_security_group" "ms_sql" {
   name        = "${var.customer}-${var.envname}-ms-sql"
   vpc_id      = "${var.vpc_id}"
   description = "ms sql security group"
-
 }
 
 resource "aws_security_group_rule" "rdp" {
-  type                      = "ingress"
-  protocol                  = "tcp"
-  from_port                 = "3389"
-  to_port                   = "3389"
-  security_group_id         = "${aws_security_group.ms_sql.id}"
-  source_security_group_id  = "${var.rdgw_external_sg_id}"
+  type                     = "ingress"
+  protocol                 = "tcp"
+  from_port                = "3389"
+  to_port                  = "3389"
+  security_group_id        = "${aws_security_group.ms_sql.id}"
+  source_security_group_id = "${var.rdgw_external_sg_id}"
 }
 
 resource "aws_security_group_rule" "sql_egress" {
-  type                      = "egress"
-  protocol                  = "tcp"
-  from_port                 = "1433"
-  to_port                   = "1433"
-  security_group_id         = "${aws_security_group.ms_sql.id}"
-  cidr_blocks = ["${var.private_subnets_cidrs}"]
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = "1433"
+  to_port           = "1433"
+  security_group_id = "${aws_security_group.ms_sql.id}"
+  cidr_blocks       = ["${var.private_subnets_cidrs}"]
 }
 
 resource "aws_security_group_rule" "sql_ingress" {
-  type                      = "ingress"
-  protocol                  = "tcp"
-  from_port                 = "1433"
-  to_port                   = "1433"
-  security_group_id         = "${aws_security_group.ms_sql.id}"
-  cidr_blocks = ["${var.private_subnets_cidrs}"]
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = "1433"
+  to_port           = "1433"
+  security_group_id = "${aws_security_group.ms_sql.id}"
+  cidr_blocks       = ["${var.private_subnets_cidrs}"]
 }
 
 resource "aws_security_group" "ms_cluster" {
   name        = "${var.customer}-${var.envname}-ms-cluster"
   vpc_id      = "${var.vpc_id}"
   description = "ms cluster security group"
-
 }
 
 resource "aws_security_group_rule" "cluster_ingress_tcp" {
-  type                      = "ingress"
-  protocol                  = "tcp"
-  from_port                 = "3343"
-  to_port                   = "3343"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = "3343"
+  to_port           = "3343"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_egress_tcp" {
-  type                      = "egress"
-  protocol                  = "tcp"
-  from_port                 = "3343"
-  to_port                   = "3343"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = "3343"
+  to_port           = "3343"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_ingress_udp" {
-  type                      = "ingress"
-  protocol                  = "udp"
-  from_port                 = "3343"
-  to_port                   = "3343"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "ingress"
+  protocol          = "udp"
+  from_port         = "3343"
+  to_port           = "3343"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_egress_udp" {
-  type                      = "egress"
-  protocol                  = "udp"
-  from_port                 = "3343"
-  to_port                   = "3343"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "egress"
+  protocol          = "udp"
+  from_port         = "3343"
+  to_port           = "3343"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_winrm_ingress_" {
-  type                      = "ingress"
-  protocol                  = "tcp"
-  from_port                 = "5985"
-  to_port                   = "5985"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = "5985"
+  to_port           = "5985"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_winrm_egress" {
-  type                      = "egress"
-  protocol                  = "tcp"
-  from_port                 = "5985"
-  to_port                   = "5985"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = "5985"
+  to_port           = "5985"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_winrms_ingress" {
-  type                      = "ingress"
-  protocol                  = "tcp"
-  from_port                 = "5986"
-  to_port                   = "5986"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = "5986"
+  to_port           = "5986"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_winrms_egress" {
-  type                      = "egress"
-  protocol                  = "tcp"
-  from_port                 = "5986"
-  to_port                   = "5986"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = "5986"
+  to_port           = "5986"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_dynamic_ingress_tcp" {
-  type                      = "ingress"
-  protocol                  = "tcp"
-  from_port                 = "49152"
-  to_port                   = "65535"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = "49152"
+  to_port           = "65535"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_dynamic_egress_tcp" {
-  type                      = "egress"
-  protocol                  = "tcp"
-  from_port                 = "49152"
-  to_port                   = "65535"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "egress"
+  protocol          = "tcp"
+  from_port         = "49152"
+  to_port           = "65535"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_dynamic_ingress_udp" {
-  type                      = "ingress"
-  protocol                  = "udp"
-  from_port                 = "49152"
-  to_port                   = "65535"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "ingress"
+  protocol          = "udp"
+  from_port         = "49152"
+  to_port           = "65535"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
 
 resource "aws_security_group_rule" "cluster_dynamic_egress_udp" {
-  type                      = "egress"
-  protocol                  = "udp"
-  from_port                 = "49152"
-  to_port                   = "65535"
-  self                      = "true"
-  security_group_id         = "${aws_security_group.ms_cluster.id}"
+  type              = "egress"
+  protocol          = "udp"
+  from_port         = "49152"
+  to_port           = "65535"
+  self              = "true"
+  security_group_id = "${aws_security_group.ms_cluster.id}"
 }
--- variables.tf
+++ variables.tf
@@ -1,17 +1,14 @@
+##Customer environment variables
+
 variable "customer" {}
-
 variable "envname" {}
-
 variable "envtype" {}
 
-variable "service" {
-  default = "rdgw"
-}
+## VPC Variables
 
-variable "public_subnets" {
-  description = "A list of public subnets inside the VPC."
-  default     = []
-}
+variable "aws_region" {}
+variable "vpc_id" {}
+variable "vpc_cidr" {}
 
 variable "private_subnets" {
   description = "A list of private subnets inside the VPC."
@@ -19,8 +16,8 @@ variable "private_subnets" {
 }
 
 variable "azs" {
-  type = "list"
-  default = ["eu-west-1a","eu-west-1b","eu-west-1c"]
+  type    = "list"
+  default = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
 }
 
 ## Userdata Variables
@@ -29,70 +26,81 @@ variable "userdata" {
   default = ""
 }
 
-
-
-# ## Launch Configuration Variables & ami lookup
+## Launch Configuration Variables & ami lookup
 
 variable "windows_ami_names" {
   default = {
-    "2008" = "Windows_Server-2008-R2_SP1-English-64Bit-Base-*",
-    "2012" = "Windows_Server-2012-R2_RTM-English-64Bit-Base-*",
-    "2016"  = "Windows_Server-2016-English-Full-Base*"
+    "2008" = "Windows_Server-2008-R2_SP1-English-64Bit-Base-*"
+    "2012" = "Windows_Server-2012-R2_RTM-English-64Bit-Base-*"
+    "2016" = "Windows_Server-2016-English-Full-Base*"
   }
 }
 
 variable "windows_ver" {}
 
+## Security group variabes
+
+variable "ads_sg" {}
+variable "rdgw_external_sg_id" {}
+variable "mgmt_internal_sg_id" {}
+
+## Volume Variables
+
+variable "disk_sizes" {
+  type = "list"
+}
+
+variable "disk_names" {
+  type = "list"
+}
+
+variable "disk_letter" {
+  type = "list"
+}
+
+variable "disk_mappings" {
+  default = ["xvdb", "xvdc", "xvdd", "xvde", "xvdf", "xvdg", "xvdh", "xvdi", "xvdj", "xvdk", "xvdl", "xvdm", "xvdn", "xvdo", "xvdp", "xvdq", "xvdr", "xvds", "xvdt", "xvdu", "xvdv", "xvdw", "xvdx", "xvdy", "xvdz"]
+}
+
+## SQL Cluster Variables
+
+variable "private_subnets_cidrs" {
+  type = "list"
+}
+
+variable "sql_cluster_azs" {}
+variable "sql_cluster_id" {}
+
+variable "sql_1_private_ip" {
+  type = "list"
+}
+
+variable "sql_2_private_ip" {
+  type = "list"
+}
+
+variable "windows_cluster_cidr" {}
+
+variable "hostname" {
+  default = ["clu1", "clu2"]
+}
+
+## Windows/AD variables
+
 variable "key_name" {}
 
 variable "admin_users" {
   default = {
-    "SimpleAD"    = "administrator",
+    "SimpleAD"    = "administrator"
     "MicrosoftAD" = "admin"
   }
 }
 
-variable "vpc_id" {}
-
-variable "ads_sg" {}
-
-variable "rdgw_external_sg_id" {}
-
-variable "disk_sizes" {type ="list"}
-variable "disk_names" {type ="list"}
-variable "disk_letter" {type ="list"}
-
-
-variable "disk_mappings" {
-  default=["xvdb","xvdc","xvdd","xvde"]
-}
-
-variable "private_subnets_cidrs" {type = "list"}
-
-
-variable "sql_cluster_azs" {}
-
-variable "sql_cluster_id" {}
-
-variable "sql_1_private_ip" {type="list"}
-variable "sql_2_private_ip" {type="list"}
-
-variable "windows_cluster_cidr" {}
-
-variable "vpc_cidr" {}
-
-variable "hostname" {default=["clu1","clu2"]}
-
 variable "domain_name" {}
-
 variable "ad_user" {}
-
 variable "domain_password" {}
-
 variable "local_password" {}
 
-variable "aws_region" {}
-
-variable "ads_dns" {type="list"}
-
-variable "mgmt_internal_sg_id" {}
+variable "ads_dns" {
+  type = "list"
+}
